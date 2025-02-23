7722843dfd62604af43fb357e4d0acaa98ca3034
MeghshamKapure <mail.meghsham@gmail.com>
2025-02-24 04:32:04 +0530

    Remove .class and .txt files from tracking and add to .gitignore

---
+++ Set_02_workingWithNumbers/WWN00_HCF_Finder.java
@@ -0,0 +1,40 @@
+package Set_02_workingWithNumbers;
+
+import java.util.Scanner;
+
+public class WWN00_HCF_Finder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number 1 : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter number 2 : ");
+        int num2 = sc.nextInt();
+
+        int hcf = 1;
+
+
+        if  (num1!=0 && num2!=0){
+            if (num1==num2){
+                hcf = num1;
+            }
+            else{
+            int smallNum = num1>num2?num1:num2;
+
+            for (int i = 2; i < smallNum; i++) {
+                if (num1 % i==0 && num2 % i ==0){
+                    hcf=i;
+                }
+            }
+            }
+        System.out.println("Highest Common Factor [HCF] for "+num1+" and "+num2+" is "+hcf);
+        }else{
+            if (num1==0)
+                System.out.println("Number 1 can't be zero");
+
+            if (num2==0)
+                System.out.println("Number 2 can't be zero");
+        }
+    }
+}
---
+++ Set_02_workingWithNumbers/WWN03_BinaryToDecimal.java
@@ -0,0 +1,45 @@
+// Binary to Decimal Conversion
+// Converts a binary number (base 2) into a decimal number (base 10).
+
+package Set_02_workingWithNumbers;
+
+import java.util.Scanner;
+
+public class WWN03_BinaryToDecimal {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the binary number : ");
+        int binaryNumber = sc.nextInt();
+        int binaryCopy = binaryNumber;
+        int base = 1;
+        int decimalNumber = 0;
+
+        if (isBinary(binaryCopy)) {
+            while (binaryNumber > 0) {
+                int digit = binaryNumber % 10;
+                digit *= base;
+                decimalNumber += digit;
+                base *= 2;
+                binaryNumber /= 10;
+            }
+
+            System.out.println("BINARY : " + binaryCopy + " is " + "DECIMAL : " + decimalNumber);
+        }
+
+    }
+
+    public static boolean isBinary(int num) {
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit > 1)
+                return false;
+
+            num /= 10;
+        }
+
+        return true;
+    }
+
+}
