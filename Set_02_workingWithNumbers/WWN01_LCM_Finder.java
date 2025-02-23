92fd2a62db57d988efcca9115a71dbd670fd2127
MeghshamKapure <mail.meghsham@gmail.com>
2025-02-24 04:46:35 +0530

    Code Commit before 24022025.05.00.00

---
+++ Set_02_workingWithNumbers/WWN01_LCM_Finder.java
@@ -0,0 +1,35 @@
+// Lowest Common Multiple (LCM)
+// Finds the smallest number that is a multiple of two or more given numbers.
+
+package Set_02_workingWithNumbers;
+
+import java.util.Scanner;
+
+public class WWN01_LCM_Finder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number 1 : ");
+        int num1 = sc.nextInt();
+ 
+        System.out.print("Enter the number 2 : ");
+        int num2 = sc.nextInt();
+
+
+        if (num2>num1){
+            int temp = num1;
+            num1 =num2;
+            num2 = temp;
+        }
+      
+        for (int i = 1; ; i++){
+            int divident = num1 * i;
+
+            if (divident % num2 == 0 ){
+                System.out.println(divident);
+                break;
+            }
+        }
+    }
+    
+}
