79c991f812459c60fc7ae62f7cd172f3ac05ed0f
MeghshamKapure <mail.meghsham@gmail.com>
2025-02-24 07:15:58 +0530

    Code Commit before 24022025.07.00.00 updated code mistake WWN06_DecimalToBinary

--- Set_02_workingWithNumbers/WWN06_DecimalToBinary.java
+++ Set_02_workingWithNumbers/WWN06_DecimalToBinary.java
@@ -1,37 +1,41 @@
-// Decimal to Binary Conversion
-// Converts a decimal number (base 10) into a binary number (base 2).
-
 package Set_02_workingWithNumbers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class WWN06_DecimalToBinary {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter the number : ");
+        System.out.print("Enter the number: ");
         int decimalValue = sc.nextInt();
 
-        ArrayList <Integer> binaryNumberAL = new ArrayList<>();
+        ArrayList<Integer> binaryNumberAL = new ArrayList<>();
 
-        while (decimalValue>0) {
-            binaryNumberAL.add(decimalValue%2);
+        while (decimalValue > 0) {
+            binaryNumberAL.add(decimalValue % 2);
             decimalValue = decimalValue / 2;
-            
-        }
-        System.out.println(binaryNumberAL.reversed());
-
-        Long binayValue=(long) 0;
-        for (int i = binaryNumberAL.size()-1 ;i>=0;i--){
-            binayValue*=10;
-            binayValue+= binaryNumberAL.get(i);
         }
 
-        System.out.println(binayValue);
+        // Reverse the list to get the correct binary order
+        Collections.reverse(binaryNumberAL);
+        
+        // Print the binary representation
+        System.out.print("Binary representation: ");
+        for (int bit : binaryNumberAL) {
+            System.out.print(bit);
+        }
+        System.out.println(); // New line for better formatting
+
+        // Alternative method: Store binary as a String
+        StringBuilder binaryString = new StringBuilder();
+        for (int bit : binaryNumberAL) {
+            binaryString.append(bit);
+        }
+
+        System.out.println("Binary as a string: " + binaryString);
 
         sc.close();
-        
     }
 }
-    
